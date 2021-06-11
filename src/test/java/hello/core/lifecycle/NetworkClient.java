f725eb5d7350fa2c5d026746318a7807efcc6ad6
heebok <lehebk@naver.com>
2021-06-11 18:02:38 +0900

    PostConstruct, PreDestroy 가장 권장하는 방법

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -16,6 +19,7 @@ public class NetworkClient {
     public void connect(){
         System.out.println("connect : " + url);
     }
+
     public void call(String message){
         System.out.println("call: " + url + " message = " + message);
     }
@@ -25,14 +29,14 @@ public class NetworkClient {
         System.out.println("close = " + url);
     }
 
-
+    @PostConstruct
     public void init(){
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
