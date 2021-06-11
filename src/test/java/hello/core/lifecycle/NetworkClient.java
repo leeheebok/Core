5721dae38390eac65b2c40fe5533dfadc3e5e5d1
heebok <lehebk@naver.com>
2021-06-11 16:45:46 +0900

    InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-
     }
 
     public void setUrl(String url){
@@ -20,11 +20,24 @@ public class NetworkClient {
         System.out.println("connect : " + url);
     }
     public void call(String message){
-        System.out.println("call: " + url + "message = " + message);
+        System.out.println("call: " + url + " message = " + message);
     }
 
     //서비스 종료시 호출
     public void disconnect(){
         System.out.println("close = " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
